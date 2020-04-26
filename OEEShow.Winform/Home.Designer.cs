e08f767e8f0af6982d9f95c9a087ee2531d94d46
Xia-Qi <davenqi@outlook.com>
2020-04-27 00:50:34 +0800

    Added OEEShow winform demo
    A demo same as OOEShow.

---
+++ OEEShow.Winform/Home.Designer.cs
@@ -0,0 +1,232 @@
+﻿namespace OEEShow.Winform
+{
+    partial class Home
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.simpleButton1 = new DevExpress.XtraEditors.SimpleButton();
+            this.gridControl1 = new DevExpress.XtraGrid.GridControl();
+            this.gridView1 = new DevExpress.XtraGrid.Views.Grid.GridView();
+            this.gridColumn1 = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.gridColumn2 = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.gridColumn12 = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.gridColumn3 = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.gridColumn4 = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.gridColumn5 = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.gridColumn6 = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.gridColumn7 = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.gridColumn8 = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.gridColumn9 = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.gridColumn10 = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.gridColumn11 = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.chartControl1 = new DevExpress.XtraCharts.ChartControl();
+            ((System.ComponentModel.ISupportInitialize)(this.gridControl1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chartControl1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // simpleButton1
+            // 
+            this.simpleButton1.Location = new System.Drawing.Point(44, 34);
+            this.simpleButton1.Name = "simpleButton1";
+            this.simpleButton1.Size = new System.Drawing.Size(121, 28);
+            this.simpleButton1.TabIndex = 0;
+            this.simpleButton1.Text = "查询";
+            this.simpleButton1.Click += new System.EventHandler(this.simpleButton1_Click);
+            // 
+            // gridControl1
+            // 
+            this.gridControl1.Location = new System.Drawing.Point(44, 68);
+            this.gridControl1.MainView = this.gridView1;
+            this.gridControl1.Name = "gridControl1";
+            this.gridControl1.Size = new System.Drawing.Size(1024, 249);
+            this.gridControl1.TabIndex = 1;
+            this.gridControl1.ViewCollection.AddRange(new DevExpress.XtraGrid.Views.Base.BaseView[] {
+            this.gridView1});
+            // 
+            // gridView1
+            // 
+            this.gridView1.Columns.AddRange(new DevExpress.XtraGrid.Columns.GridColumn[] {
+            this.gridColumn1,
+            this.gridColumn2,
+            this.gridColumn12,
+            this.gridColumn3,
+            this.gridColumn4,
+            this.gridColumn5,
+            this.gridColumn6,
+            this.gridColumn7,
+            this.gridColumn8,
+            this.gridColumn9,
+            this.gridColumn10,
+            this.gridColumn11});
+            this.gridView1.GridControl = this.gridControl1;
+            this.gridView1.Name = "gridView1";
+            // 
+            // gridColumn1
+            // 
+            this.gridColumn1.Caption = "工序号";
+            this.gridColumn1.FieldName = "Number";
+            this.gridColumn1.Name = "gridColumn1";
+            this.gridColumn1.Visible = true;
+            this.gridColumn1.VisibleIndex = 0;
+            // 
+            // gridColumn2
+            // 
+            this.gridColumn2.Caption = "设备编码";
+            this.gridColumn2.FieldName = "Equipment.Code";
+            this.gridColumn2.Name = "gridColumn2";
+            this.gridColumn2.Visible = true;
+            this.gridColumn2.VisibleIndex = 1;
+            // 
+            // gridColumn12
+            // 
+            this.gridColumn12.Caption = "设备名称";
+            this.gridColumn12.FieldName = "Equipment.Name";
+            this.gridColumn12.Name = "gridColumn12";
+            this.gridColumn12.Visible = true;
+            this.gridColumn12.VisibleIndex = 2;
+            // 
+            // gridColumn3
+            // 
+            this.gridColumn3.Caption = "日产量";
+            this.gridColumn3.FieldName = "FormatProduction";
+            this.gridColumn3.Name = "gridColumn3";
+            this.gridColumn3.Visible = true;
+            this.gridColumn3.VisibleIndex = 3;
+            // 
+            // gridColumn4
+            // 
+            this.gridColumn4.Caption = "不合格数量";
+            this.gridColumn4.FieldName = "FormatRejects";
+            this.gridColumn4.Name = "gridColumn4";
+            this.gridColumn4.Visible = true;
+            this.gridColumn4.VisibleIndex = 4;
+            // 
+            // gridColumn5
+            // 
+            this.gridColumn5.Caption = "运行时间（秒）";
+            this.gridColumn5.FieldName = "RunTime";
+            this.gridColumn5.Name = "gridColumn5";
+            this.gridColumn5.Visible = true;
+            this.gridColumn5.VisibleIndex = 5;
+            // 
+            // gridColumn6
+            // 
+            this.gridColumn6.Caption = "计划停机时间（秒）";
+            this.gridColumn6.FieldName = "PlanHaltTime";
+            this.gridColumn6.Name = "gridColumn6";
+            this.gridColumn6.Visible = true;
+            this.gridColumn6.VisibleIndex = 6;
+            // 
+            // gridColumn7
+            // 
+            this.gridColumn7.Caption = "理论节拍（秒每件）";
+            this.gridColumn7.FieldName = "FormatTheoreticallyMeter";
+            this.gridColumn7.Name = "gridColumn7";
+            this.gridColumn7.Visible = true;
+            this.gridColumn7.VisibleIndex = 7;
+            // 
+            // gridColumn8
+            // 
+            this.gridColumn8.Caption = "时间稼动率";
+            this.gridColumn8.FieldName = "RateOfTimeMovement";
+            this.gridColumn8.Name = "gridColumn8";
+            this.gridColumn8.Visible = true;
+            this.gridColumn8.VisibleIndex = 8;
+            // 
+            // gridColumn9
+            // 
+            this.gridColumn9.Caption = "性能稼动率";
+            this.gridColumn9.FieldName = "PerformanceYield";
+            this.gridColumn9.Name = "gridColumn9";
+            this.gridColumn9.Visible = true;
+            this.gridColumn9.VisibleIndex = 9;
+            // 
+            // gridColumn10
+            // 
+            this.gridColumn10.Caption = "合格率";
+            this.gridColumn10.FieldName = "Yield";
+            this.gridColumn10.Name = "gridColumn10";
+            this.gridColumn10.Visible = true;
+            this.gridColumn10.VisibleIndex = 10;
+            // 
+            // gridColumn11
+            // 
+            this.gridColumn11.Caption = "全局设备效率";
+            this.gridColumn11.FieldName = "GlobalPlantEfficiency";
+            this.gridColumn11.Name = "gridColumn11";
+            this.gridColumn11.Visible = true;
+            this.gridColumn11.VisibleIndex = 11;
+            // 
+            // chartControl1
+            // 
+            this.chartControl1.Legend.Name = "Default Legend";
+            this.chartControl1.Location = new System.Drawing.Point(44, 323);
+            this.chartControl1.Name = "chartControl1";
+            this.chartControl1.SeriesSerializable = new DevExpress.XtraCharts.Series[0];
+            this.chartControl1.Size = new System.Drawing.Size(1024, 366);
+            this.chartControl1.TabIndex = 2;
+            this.chartControl1.Click += new System.EventHandler(this.chartControl1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 14F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1117, 734);
+            this.Controls.Add(this.chartControl1);
+            this.Controls.Add(this.gridControl1);
+            this.Controls.Add(this.simpleButton1);
+            this.Name = "Form1";
+            this.Text = "主页 - OEE";
+            ((System.ComponentModel.ISupportInitialize)(this.gridControl1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chartControl1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraEditors.SimpleButton simpleButton1;
+        private DevExpress.XtraGrid.GridControl gridControl1;
+        private DevExpress.XtraGrid.Views.Grid.GridView gridView1;
+        private DevExpress.XtraGrid.Columns.GridColumn gridColumn1;
+        private DevExpress.XtraGrid.Columns.GridColumn gridColumn2;
+        private DevExpress.XtraGrid.Columns.GridColumn gridColumn3;
+        private DevExpress.XtraGrid.Columns.GridColumn gridColumn4;
+        private DevExpress.XtraGrid.Columns.GridColumn gridColumn5;
+        private DevExpress.XtraGrid.Columns.GridColumn gridColumn6;
+        private DevExpress.XtraGrid.Columns.GridColumn gridColumn7;
+        private DevExpress.XtraGrid.Columns.GridColumn gridColumn8;
+        private DevExpress.XtraGrid.Columns.GridColumn gridColumn9;
+        private DevExpress.XtraGrid.Columns.GridColumn gridColumn10;
+        private DevExpress.XtraGrid.Columns.GridColumn gridColumn11;
+        private DevExpress.XtraGrid.Columns.GridColumn gridColumn12;
+        private DevExpress.XtraCharts.ChartControl chartControl1;
+    }
+}
+
---
+++ OEEShow.Winform/Home.cs
@@ -0,0 +1,80 @@
+﻿using DevExpress.XtraCharts;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace OEEShow.Winform
+{
+    public partial class Home : DevExpress.XtraEditors.XtraForm
+    {
+        public Home()
+        {
+            InitializeComponent();
+        }
+
+        private void simpleButton1_Click(object sender, EventArgs e)
+        {
+            var data = new Acme.Core.EquipmentManagement.WebService.Impl.EquipmentManagement().GetProcedures();
+
+            gridControl1.DataSource = data;
+            DataSet ds = new DataSet();
+            var tb = new DataTable();
+            tb.Columns.Add("Number");
+            tb.Columns.Add("Equipment.Code");
+            tb.Columns.Add("Equipment.Name");
+            tb.Columns.Add("FormatProduction");
+            tb.Columns.Add("FormatRejects");
+            tb.Columns.Add("RunTime");
+            tb.Columns.Add("PlanHaltTime");
+            tb.Columns.Add("FormatTheoreticallyMeter");
+            tb.Columns.Add("RateOfTimeMovement");
+            tb.Columns.Add("PerformanceYield");
+            tb.Columns.Add("Yield");
+            tb.Columns.Add("GlobalPlantEfficiency");
+            foreach (var a in data)
+            {
+                var newRow = tb.NewRow();
+
+                newRow.ItemArray = new object[] {
+                    a.Number,
+                    a.Equipment.Code, a.Equipment.Name,
+                    a.Production, a.Rejects, a.RunTime, a.PlanHaltTime, a.TheoreticallyMeter, a.RateOfTimeMovement, a.PerformanceYield, a.Yield, a.GlobalPlantEfficiency
+                };
+                tb.Rows.Add(newRow);
+            }
+            ds.Tables.Add(tb);
+            
+
+            gridControl1.DataSource = ds.Tables[0];
+
+
+            IList<OOEChartData> formatData = new List<OOEChartData>();
+            foreach (var procedure in data)
+            {
+                formatData.Add(new OOEChartData() { TypeName = "时间稼动率", Value = procedure.RateOfTimeMovement, EquipmentName = procedure.Equipment.Name });
+                formatData.Add(new OOEChartData() { TypeName = "性能稼动率", Value = procedure.PerformanceYield, EquipmentName = procedure.Equipment.Name });
+                formatData.Add(new OOEChartData() { TypeName = "合格率", Value = procedure.Yield, EquipmentName = procedure.Equipment.Name });
+                formatData.Add(new OOEChartData() { TypeName = "全局设备效率", Value = procedure.GlobalPlantEfficiency, EquipmentName = procedure.Equipment.Name });
+            }
+            chartControl1.BindToData(ViewType.Bar, formatData, "TypeName", "EquipmentName", "Value");
+            //chartControl1.Series[0].Points.Add(new SeriesPoint("TypeName",)
+
+        }
+        private class OOEChartData
+        {
+            public string TypeName { get; set; }
+            public decimal Value { get; set; }
+            public string EquipmentName { get; set; }
+        }
+
+        private void chartControl1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
